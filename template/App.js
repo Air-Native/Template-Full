fc02147e202569915cfbe35c1ad5fe397d527512
slimecube98 <57504261+slimecube98@users.noreply.github.com>
2023-04-26 22:41:07 +0300

    fix permissions and increase memory

--- template/App.js
+++ template/App.js
@@ -45,7 +45,7 @@ OneSignal.setAppId('d011336f-f960-4b1a-ad58-703a6bba0d46');
  *  будет фулскрин приложение без шторки
  */
 const setFullscreenWithoutBar = false;
-const USER_AGENT = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/5322 (KHTML, like Gecko) Chrome/37.0.893.0 Mobile Safari/5322";
+const USER_AGENT = "Mozilla/5.0 (Linux; Android 5.0.1; Nokia 1000 wifi Build/GRK39F) AppleWebKit/533.12 (KHTML, like Gecko)  Chrome/50.0.1011.255 Mobile Safari/600.7";
 
 /** Если поставить
  *  setFullscreenWithBar = true
