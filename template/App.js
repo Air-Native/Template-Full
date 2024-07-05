b1f8962df56287c603647e3d2b198fe9f2b61e5c
slimecube98 <slimecube98@gmail.com>
2024-07-05 13:07:53 +0300

    App Functionality (new): Add keep awake

--- template/App.js
+++ template/App.js
@@ -25,6 +25,7 @@ import Geolocation from '@react-native-community/geolocation';
 import RNBootSplash from 'react-native-bootsplash';
 import {URL} from 'react-native-url-polyfill';
 import changeNavigationBarColor from 'react-native-navigation-bar-color';
+import KeepAwake from 'react-native-keep-awake';
 import Player from './controllers/Player'
 
 const PlayerInstance = new Player()
@@ -180,6 +181,9 @@ class App extends Component {
     this.invoke.define('getDeviceOS', this.getDeviceOS);
     this.invoke.define('showPrompt', this.showPrompt);
     this.invoke.define('getPermissionsUser', this.getPermissionsUser);
+
+    this.invoke.define('keepAwake', this.changeKeepAwake);
+
     if (this.state.contactsEnabled) {
       this.invoke.define('getContacts', this.getContacts);
     }
@@ -233,6 +237,15 @@ class App extends Component {
     }
     this.appStateChecker.remove();
   }
+
+  /** Enable Keep Awake */
+  changeKeepAwake = (shouldBeAwake) => {
+    if (shouldBeAwake) {
+      KeepAwake.activate();
+    } else {
+      KeepAwake.deactivate();
+    } 
+  };
 
   /** Platform OS */
   getDeviceOS = () => {
