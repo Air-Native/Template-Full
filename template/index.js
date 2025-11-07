5ae5405855c52dda2c2a728c1578d4cce3e0a371
slimecube98 <slimecube98@gmail.com>
2025-11-07 19:59:28 +0200

    ddd

--- template/index.js
+++ template/index.js
@@ -1,8 +1,9 @@
-import {AppRegistry} from 'react-native';
-import TrackPlayer from 'react-native-track-player';
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
-import PlaybackService from './util/playerPlaybackService'
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
-TrackPlayer.registerPlaybackService( () => PlaybackService )
--- template/ios/dummy.swift
+++
@@ -1,8 +0,0 @@
-//
-//  dummy.swift
-//  ProjectName
-//
-//  Created by Dmitrii Vasiliev on 26.04.2023.
-//
-
-import Foundation
